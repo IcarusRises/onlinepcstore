ed1dcef327a16f8712e27083d8347f633b2c7ef5
Billie Tran <0billtran@gmail.com>
2019-03-28 06:11:22 -0700

    Creation of mongo Schema for laptops

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-let laptops = [
-    {image: "img/PC/probook.jpg", name: 'HP ProBook 450 G3', orignal: "469.99", savings:"170", price: "399.99"},
-    {image: "img/PC/probook.jpg", name: 'HP ProBook 450 G2', orignal: "479.99", savings:"50", price: "599.99"},
-    {image: "img/PC/probook.jpg", name: 'HP ProBook 450 G4', orignal: "569.99", savings:"60", price: "650.00"},
-    {image: "img/PC/probook.jpg", name: 'HP ProBook 450 G5', orignal: "499.99", savings:"85", price: "469.99"}
-]
+const mongoose = require("mongoose");
 const port = 3000;
+
+//SCHEMA
+const laptopSchema = new mongoose.Schema({
+    image: String,
+    name: String,
+    original: Number,
+    savings: Number,
+    price: Number
+});
+
+const Laptop = mongoose.model("Laptop", laptopSchema); 
+
+// Laptop.create({
+//     image: 'img/PC/probook.jpg',
+//     name: 'HP ProBook 450 G3',
+//     original: "469.99",
+//     savings: "170",
+//     price: "399.99"
+// }, function(err, laptop){
+//     if(err){
+//         console.log(err);
+//         console.log("ERROR");
+//     } else {
+//         console.log("SUCCESS");
+//         console.log(laptop)
+//     }
+// });
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/pcstore",{useNewUrlParser: true});
 
 
 // HOME PAGE
-app.get("/", function(req, res){
-    res.render("homepage", {laptops : laptops});
+app.get("/",function(req, res){
+    Laptop.find({}, function(err, allLaptops){
+        if(err){
+            console.log("ERROR");
+            console.log(err);
+        } else {
+            res.render("homepage", {laptops : allLaptops})
+        }
+    })
 })
 
 app.get("/laptops", function(req, res){
@@ -39,7 +69,13 @@ app.post("/", function(req, res){
     let savings = req.body.savings;
     let price = req.body.price;
     let newLaptops = { image: image, name: name, original: original, savings: savings, price: price};
-    laptops.push(newLaptops);
+    Laptop.create(newLaptops, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
     res.redirect("/");
 })
 
@@ -48,6 +84,7 @@ app.post("/", function(req, res){
 app.get("/new", function(req, res){
     res.render("new");
 });
+
 
 
 //LISTEN PORT
