2549397b4c3d00122fb45108ed6cae805d961a90
Billie Tran <0billtran@gmail.com>
2019-04-16 03:24:58 -0700

    Refactored Routes

---
+++ routes/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const nodeMailer = require("nodemailer");
+const Laptop = require("../models/laptops");
+
+// HOME PAGE
+router.get("/",function(req, res){
+    Laptop.find({}, function(err, allLaptops){
+        if(err){
+            console.log("ERROR");
+            console.log(err);
+        } else {
+            res.render("homepage", {laptops : allLaptops})
+        }
+    })
+});
+
+// POST ROUTE
+router.post("/", function(req, res){
+    let brand = req.body.brand;
+    let image = req.body.image;   
+    let name = req.body.name;
+    let original = req.body.original;
+    let savings = req.body.savings;
+    let price = req.body.price;
+    let newLaptops = { brand: brand, image: image, name: name, original: original, savings: savings, price: price};
+    Laptop.create(newLaptops, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+//FAQ
+router.get("/faq", function(req, res){
+    res.render("faq");
+});
+
+//ABOUT
+router.get("/about", function(req, res){
+    res.render("about");
+});
+
+//CONTACT
+router.get("/contact",function(req, res){
+    res.render("contact");
+});
+
+router.post("/contact",function(req,res){
+    let mailOpts, smtpTrans;
+    smtpTrans = nodeMailer.createTransport({
+        host: changeThisLater,
+        port: changeThisLater,
+        secure: true,
+        auth: {
+            user: changeThisLater,
+            pass: changeThisLater
+        }
+    });
+    mailOpts = {
+        from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+        to: changeThisLater,
+        subject: "New message from contact form at Linhs PC Store",
+        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts, function(err, res){
+        if(err){
+            res.render("contact-failure");
+        } else {
+            res.render("contact-success");
+        }
+    });
+});
+
+//PURCHASE
+router.get("/purchase",function(req, res){
+    res.render("purchase");
+});
+
+//MANAGER LOGIN
+router.get("/login",function(req, res){
+    res.render("login");
+});
+
+
+module.exports = router;
---
+++ routes/laptop.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const Laptop = require("../models/laptops");
+
+
+//LAPTOPS: Shows Every Laptop
+router.get("/", function(req, res){
+    res.render("laptops");
+})
+
+
+//LAPTOP: Individual Laptops 
+router.get("/:id", function(req, res){
+    Laptop.findById(req.params.id, function(err,laptop){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("laptop", {laptop: laptop});
+        }
+    })
+});
+
+//LAPTOP CREATION FORM
+router.get("/new", function(req, res){
+    res.render("new");
+});
+
+//LAPTOP EDIT
+router.get("/laptops/:id/edit", function(req, res){
+    Laptop.findById(req.params.id, function(err,foundLaptop){
+        if(err){
+            console.log("Error: " + err );
+            res.redirect("/");
+        } else {
+            res.render("edit", {laptop: foundLaptop});
+        }
+    });
+});
+
+//LAPTOP UPDATE
+router.put("/laptops/:id", function(req, res){
+    Laptop.findOneAndUpdate(req.params.id, req.body.laptop, function(err, updatedLaptop){
+        if(err){
+            res.redirect("/");
+        } else {
+            res.redirect("/laptops/" + req.params.id);
+            console.log(updatedLaptop);
+        }
+    });
+});
+
+//LAPTOP DESTROY
+router.delete("/:id", function(req, res){
+    Laptop.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+module.exports = router;
