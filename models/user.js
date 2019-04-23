77231e00bc9536707b9e3035db77c1ab5d64a2d5
Billie Tran <0billtran@gmail.com>
2019-04-23 06:53:26 -0700

    Creation of user schema and modules

--- models/user.js
+++ models/user.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt = require('bcryptjs');
 
+//USER SCHEMA
 const userSchema = new mongoose.Schema({
-    username : String,
+    username: String,
     password: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
+
+module.exports.createUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(newUser.password, salt, function(err, hash) {
+        newUser.password = hash;
+        newUser.save(callback);
+      });
+    });
+  };
+
+  module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username};
+    User.findOne(query, callback);
+  };
+  
+  module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+  };
+  
+  module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+      if(err) throw err;
+      callback(null, isMatch);
+    });
+  };
